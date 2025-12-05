44f209b34f2c9cc17a8623b38e9dd51ded60e007
ryang-commits <ryan.g@escolar.ifrn.edu.br>
2025-12-05 20:39:08 -0300

    desenvolvimento do projeto 2 do terceiro bimestre da diciplina de PPI

---
+++ login-mvc/app/views/protect.php
@@ -0,0 +1,14 @@
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Acesso Negado</title>
+</head>
+<body>
+
+<h2>Você não está autenticado!</h2>
+<a href="../../index.php">Ir para o Login</a>
+
+</body>
+</html>
